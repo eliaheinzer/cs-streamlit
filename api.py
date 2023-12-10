b2bb119076e7fe4b1f8df7b72e7092306776c8ef
Philipp <philippjenny@bluewin.ch>
2023-12-10 16:52:14 +0100

    Removed 5 second delay

--- api.py
+++ api.py
@@ -4,29 +4,33 @@ import html
 
 
 def get_question(category, difficulty):
-    
+    #getting 5 questions at the time so the request delay is not a problem
+    amount = 5
+    trivia_packet = []
     #if the question type is changed than the answer extraction needs an update
     #the category type is a number not the name
-    params ={"amount":1,"category":category,"difficulty":difficulty, "type":"multiple"}
+    params ={"amount":amount,"category":category,"difficulty":difficulty, "type":"multiple"}
     url ="https://opentdb.com/api.php?"
     api_answer = requests.get(url, params = params)
     
     #unpacking the json
     answer = api_answer.json()
     
-    #changing the element in to a readable result
-    question_with_html = answer['results'][0]['question']
-    question = html.unescape(question_with_html)
+    #changing the element in to a readable result and distributing each in a tuple
+    for i in range(0,amount):
+        question_with_html = answer['results'][i]['question']
+        question = html.unescape(question_with_html)
     
-    wrong_answers_with_html = answer['results'][0]['incorrect_answers']
-    wrong_answers = html.unescape(wrong_answers_with_html)
+        wrong_answers_with_html = answer['results'][i]['incorrect_answers']
+        wrong_answers = html.unescape(wrong_answers_with_html)
     #This gives a list with the wrong answers
     
-    correct_answer_with_html = answer['results'][0]['correct_answer']
-    correct_answer = html.unescape(correct_answer_with_html)
-    
+        correct_answer_with_html = answer['results'][i]['correct_answer']
+        correct_answer = html.unescape(correct_answer_with_html)
+        question_packet= (question, wrong_answers, correct_answer)
+        trivia_packet.append(question_packet)
     #putting all the results in a tuple
-    trivia_packet = (question, wrong_answers, correct_answer)
+    
     return trivia_packet
     #example response:('Who painted The Starry Night?',['Pablo Picasso', 'Leonardo da Vinci', 'Michelangelo'],'Vincent van Gogh')
 
--- trivia.py
+++ trivia.py
@@ -25,7 +25,7 @@ if player_name:
     
 #choose a category   
 
-st.session_state['category'] = st.selectbox("Pick a category:", ["9", "10", "11"], index=0, key='category_select')
+st.session_state['category'] = st.selectbox("Pick a category:", ["9", "10", "25"], index=0, key='category_select')
 st.session_state['difficulty'] = st.selectbox("Pick a difficulty:", ["easy", "medium", "hard"], index=0, key='difficulty_select')
 if "highscore" not in st.session_state:
     st.session_state["highscore"] = 0
@@ -34,29 +34,37 @@ if 'score' not in st.session_state:
     st.session_state['score']= 0
 
 
-#start_game = st.button("CLICK HERE TO START THE GAME")
-
-
-#game_over = #muss noch definiert werden
-
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-        # DELETE THIS CODE, JUST FOR TESTING!!!
 
 def get_new_question():
     category = st.session_state["category"]
     difficulty = st.session_state["difficulty"]
 
-    #importing the results from the api
-    api_result = get_question(category, difficulty)
-    st.session_state["question"] = api_result[0]
-    st.session_state["answers"] = api_result[1] + [api_result[2]]
-    st.session_state["correct_answer"] = api_result[2]
+    #importing the results from the api if all the current questions are used
+    if st.session_state['question_counter'] == 0:
+        st.session_state['api_result'] = get_question(category, difficulty)
+
+    #breaking up the results packet
+    current_question_packet = st.session_state['api_result'][st.session_state['question_counter']]
+    
+    st.session_state["question"] = current_question_packet[0]
+
+    #both parts are lists before concatenating
+    incorrect_answers = current_question_packet[1]
+    correct_answer = current_question_packet[2]
+
+    st.session_state["correct_answer"] = correct_answer
+
+    # Ensure all answers are strings and combine them
+    st.session_state["answers"] = [str(answer) for answer in incorrect_answers + [correct_answer]]
+
 
     #getting rid of the fixed order
     random.shuffle(st.session_state["answers"])
     st.session_state['selected_answer'] = None
+    st.session_state['question_counter'] += 1
+    if st.session_state['question_counter'] == 5:
+        st.session_state['question_counter'] = 0
+
 
 def display_question():
     st.subheader(st.session_state["question"])
@@ -74,13 +82,13 @@ def display_question():
 def process_answer():
     #Check if answer is correct
     if st.session_state['selected_answer'] == st.session_state["correct_answer"]:
-        st.write("Correct Answer! Congrats!")
+        st.success("Correct Answer! Congrats!")
         st.session_state['score'] += 1
         get_new_question()
     #subtract a live if answer is wrong
     else:
         st.session_state['lives'] -= 1
-        st.write(f"Wrong, you have {st.session_state['lives']} lives left!")
+        st.error(f"Wrong, you have {st.session_state['lives']} lives left!")
             #if there are no lives left in this session end this round otherwise restart
         if st.session_state['lives'] <= 0:
             st.write(f"Final Score: {st.session_state['score']}")
@@ -105,8 +113,10 @@ if st.button("Start Game"):
     st.session_state['start_game'] = True
     st.session_state['lives'] = 3
     st.session_state['score'] = 0
+    st.session_state['question_counter'] = 0
     #get question
     get_new_question()
+
 
 #actual process of the code starts
 if st.session_state['start_game']:
@@ -118,55 +128,4 @@ if st.session_state['start_game']:
 #Display score and highscore
 st.write(f'Your score:{st.session_state["score"]}')
 st.write(f'Your Highscore is:{st.session_state["highscore"]}')
-
-
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-        # DELETE THIS CODE, JUST FOR TESTING!!!
-
-
-            
-# if start_game:
-#     category = st.session_state["category"]
-#     difficulty = st.session_state["difficulty"]
-#     lives = 3
-#     score = 0
-#     #setting the lives and score to the starting values
-#     while lives > 0:
-#         api_result = get_question(category, difficulty)
-#         question = api_result[0]
-#         correct_answer = api_result[2]
-#         #wrong_answer = #muss noch definiert werden
-#         answers = api_result[1]
-#         answers.append(correct_answer)
-
-
-#         st.subheader(question)
-        
-#         random.shuffle(answers)
-
-#         if 'selected_answer' not in st.session_state:
-#             st.session_state['selected_answer'] = None
-
-#         for ans in answers:
-#             if st.button(ans, key=ans):
-#                 st.session_state['selected_answer'] = ans
-#                 # Break after a button is clicked
-
-#         #mehrere Probleme: streamlit führt die seite jeweils neu aus nachdem etwas geklickt wird. (machbar aber kompliziert mit session_state
-#                         #hauptproblem: sobald ein knopf gedrückt wird wird nicht wie geplant etwas angezeigt sondern das spiel wird zurück gesetzt. 
-                        
-        
-#         if st.session_state['selected_answer'] is not None:  
-#             if st.session_state['selected_answer'] == correct_answer:
-#                 st.write("Correct Answer! Congrats!")
-
-#             else:
-#                 st.write("Wrong, GAME OVER!")
-
-
-
-#             else:
-#                 st.write("Wrong, GAME OVER!")
 
